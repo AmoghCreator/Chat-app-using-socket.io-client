59f015df19dafe5b1b368bc17f316d0ad3ed3259
AmoghCreator <amoghpreneur@gmail.com>
2024-02-14 17:09:17 +0530

    full and final

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8080';
+const URL = 'https://elabs-karnival-api.el.r.appspot.com/';
 
 const socket = io.connect(URL);
 
